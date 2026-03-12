1044ce03acfaae583e2b3706ab697165d330e51d
Queiar <frio.alastor@gmail.com>
2026-03-12 12:43:14 -0600

    Mi primera web

---
+++ script.js
@@ -0,0 +1,6 @@
+function bootSystem() {
+    const titulo = document.getElementById('texto-principal');
+    titulo.innerText = "¡VIBE CODING ACTIVO! 🚀";
+    titulo.style.color = "#7ef173";
+    console.log("Sistema Queiar respondiendo desde Pop!_OS");
+}
